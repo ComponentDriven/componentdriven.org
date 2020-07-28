bc5c116dcdcc9d983482ead0d1d7c7a7b5022ff2
winkerVSbecks <varunvachhar@gmail.com>
2020-07-28 15:23:32 -0400

    use next preset for storybook

---
+++ .storybook/main.js
@@ -0,0 +1,8 @@
+module.exports = {
+  stories: ['../src/**/*.stories.tsx'],
+  addons: [
+    '@next/plugin-storybook/preset',
+    '@storybook/addon-actions',
+    '@storybook/addon-links',
+  ],
+};
