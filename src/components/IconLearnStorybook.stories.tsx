5f319344330cf4f3abe7da9dc2cb27dc71228eab
winkerVSbecks <varunvachhar@gmail.com>
2020-07-28 15:24:24 -0400

    add IconLearnStorybook

---
+++ src/components/IconLearnStorybook.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { IconLearnStorybook } from './IconLearnStorybook';
+
+export default {
+  component: IconLearnStorybook,
+  title: 'Basics/IconLearnStorybook',
+};
+
+export const Default = () => <IconLearnStorybook width="80" height="80" />;
