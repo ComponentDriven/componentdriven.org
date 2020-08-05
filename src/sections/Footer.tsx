a28849ac7fa9e394452a789c1fc7aa58d0a426af
winkerVSbecks <varunvachhar@gmail.com>
2020-08-05 17:23:03 -0400

    footer margin

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -6,8 +6,12 @@ import { margin } from '../styles';
 
 const FooterContainer = styled(Text).attrs({ as: 'footer' })`
   color: #586368;
-  margin-bottom: ${margin.medium}px;
+  margin-bottom: 3rem;
   text-align: center;
+
+  @media (min-width: ${styles.breakpoint}px) {
+    margin-bottom: ${margin.medium}px;
+  }
 `;
 
 const FooterLink = styled(Link).attrs({ tertiary: true })`
