db0eb62a556aa3330d5a833a3ffd9ea1d6fab238
winkerVSbecks <varunvachhar@gmail.com>
2020-08-05 11:28:45 -0400

    favicons and open graph

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,7 +16,14 @@ function MyApp({ Component, pageProps }) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="alternate icon" href="/favicon.png" type="image/png" />
+
+        {/* Performance */}
+        <link rel="preconnect" href="https://api.github.com" />
+        <link rel="dns-prefetch" href="//fonts.googleapis.com" />
+        {/* @ts-ignore */}
+        <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin />
         <link href={fontUrl} rel="stylesheet" />
       </Head>
       <StorybookDSGlobalStyle />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,3 @@
-import Head from 'next/head';
 import { Header } from '../src/sections/Header';
 import { Why } from '../src/sections/Why';
 import { How } from '../src/sections/How';
@@ -6,13 +5,17 @@ import { Benefits } from '../src/sections/Benefits';
 import { Divider } from '../src/components/Divider';
 import { Tools } from '../src/sections/Tools/Tools';
 import { Footer } from '../src/sections/Footer';
+import SEO from '../src/components/SEO';
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Component Driven</title>
-      </Head>
+      <SEO
+        title="Component Driven User Interfaces"
+        description="The development and design practice of building user interfaces with modular components. UIs are built from the “bottom up” starting with basic components then progressively combined to assemble screens."
+        url="https://componentdriven.org/"
+        image="https://componentdriven.org/og-component-driven.jpg"
+      />
       <Header />
       <Why />
       <How />
