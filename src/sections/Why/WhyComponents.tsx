249f9bd56c9eaa513722d8437095023c09ec7acb
Dominic Nguyen <dom@hichroma.com>
2020-08-09 19:43:47 -0400

    Simplify language to make it easier to understand

--- src/sections/Why/WhyComponents.tsx
+++ src/sections/Why/WhyComponents.tsx
@@ -11,20 +11,18 @@ export const WhyComponents = () => (
     </Heading>
     <Stack axis="vertical" space="xsmall">
       <Text>
-        Modern user interfaces are more complicated than ever. Customers expect
-        compelling, personalized experiences delivered across devices and
-        channels. That means frontend developers and designers have to embed
-        more logic and state into the UI.
+        Modern user interfaces are more complicated than ever. People expect
+        compelling, personalized experiences delivered across devices. That means frontend developers and designers have to embed
+        more logic into the UI.
       </Text>
       <Text>
-        But managing UIs becomes unwieldy as applications grow. Large UIs are
-        brittle, painful to debug, and time consuming to ship. Modular
-        construction with components makes it easy to build robust yet
-        configurable UIs. The component model encourages interchangeability by
-        isolating component state from an application’s business logic.
+        But UIs become unwieldy as applications grow. Large UIs are
+        brittle, painful to debug, and time consuming to ship. Breaking them down in a modular way makes it easy to build robust yet
+        flexible UIs. 
       </Text>
       <Text>
-        It decomposes complex screens into simple, standardized components. Each
+        Components enable interchangeability by isolating state from application business logic.
+        That way, you can decompose complex screens into simple components. Each
         component has a well-defined API and fixed series of states that are
         mocked. This allows components to be taken apart and recomposed to build
         different UIs.
